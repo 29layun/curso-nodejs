e75b2947f07ffe69001785b4bcd1802cab2cf29a
Miguel jimenez <mjimenezt003@alumno.uaemex.mx>
2023-04-19 13:23:07 -0600

    Actualizando contenido de math/index.js

--- math/index.js
+++ math/index.js
@@ -1,3 +1,5 @@
+//Formas comunes de importar 
+/*
 function add(x, y) {
     // contenido de la funcion sumar
     return x + y
@@ -18,6 +20,7 @@ function divide(x, y) {
     return x / y
 }
 
+*/
 //Para visualizar en consola
 /* 
 console.log(add(10, 20))
@@ -26,11 +29,55 @@ console.log(multiply(10, 20))
 console.log(divide(10, 20))
 */
 
-//Exportar por modulos
-
+//Exportar por modulos de manera comun
+/*
 module.exports = {
     add,
     subtract,
     multiply,
     divide
 }
+*/
+
+//Forma de exportar modulos actualizado
+//Se puede exportar de manera individual cada funcion con agregandole export como en las funciones siguientes 
+export function add(x, y) {
+    // contenido de la funcion sumar
+    return x + y
+}
+
+export function subtract(x, y) {
+    // contenido de la funcion restar
+    return x - y
+}
+
+export function multiply(x, y) {
+    // contenido de la funcion multiplicar
+    return x * y
+}
+
+export function divide(x, y) {
+    // contenido de la funcion dividir
+    return x / y
+}
+
+
+//Para visualizar en consola
+/* 
+console.log(add(10, 20))
+console.log(subtract(10, 20))
+console.log(multiply(10, 20))
+console.log(divide(10, 20))
+*/
+
+//Exportar por modulos de manera actualizada
+//Para exportar las funciones de manera general(grupal) se usa el siguiente comando
+export default {
+    add,
+    subtract,
+    multiply,
+    divide
+}
+
+
+
